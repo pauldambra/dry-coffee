10c2a6bbf909f5558bc3c7c8577f5b1fe62431f7
pauldambra <paul.dambra@gmail.com>
2018-03-30 19:31:37 +0100

    add chocolate sprinkles and tidy up with gitignore

--- src/dry-coffee.kt
+++ src/dry-coffee.kt
@@ -1,20 +1,32 @@
+open class Drink(
+  val name: String,
+  val milk: Int = 0,
+  val sugar: Int = 0,
+  val warmMilk: Int = 0,
+  val chocolateSprinkles: Int = 0
+) {
 
-
-
-open class Drink(val name: String, val milk: Int, val sugar: Int) {
     fun pour(): Drink = this
-    override fun toString(): String {
-        return "$name(milk=$milk, sugar=$sugar)"
-    }
+
+    override fun toString()
+      = "Drink(" +
+      "name='$name', " +
+      "milk=$milk, " +
+      "sugar=$sugar, " +
+      "warmMilk=$warmMilk, " +
+      "chocolateSprinkles=$chocolateSprinkles)"
 }
 
-class Coffee(milk: Int, sugar: Int) : Drink("Coffee", milk, sugar)
-
-class Tea(milk: Int, sugar: Int) : Drink("Tea", milk, sugar)
-
 fun main(args: Array<String>) {
-    val whiteCoffeeNoSugar = Coffee(1, 0).pour()
-    val buildersTea = Tea(1,3).pour()
+    val whiteCoffeeNoSugar = Drink(name = "Coffee", milk = 1, sugar = 0).pour()
     println("poured $whiteCoffeeNoSugar")
+
+    val buildersTea = Drink(name = "Tea", milk = 1, sugar = 3).pour()
     println("poured $buildersTea")
+
+    val warmerCoffee = Drink(name = "Warm coffee", sugar = 2, warmMilk = 2).pour()
+    println("poured $warmerCoffee")
+
+    val mocha = Drink(name = "Mocha", sugar = 2, warmMilk = 2, chocolateSprinkles = 3).pour()
+    println("poured $mocha")
 }
